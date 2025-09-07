dfdf7d22d1a05bcc7643a54bf3fca3baa40123fe
Kelvin Ogboriauphien <kelvinogboriauphien@Kelvins-MacBook-Pro.local>
2025-09-08 01:28:49 +0200

    starting Projects

--- src/sections/Projects.jsx
+++ src/sections/Projects.jsx
@@ -2,7 +2,9 @@ import React from 'react'
 
 const Projects = () => {
     return (
-        <div>Projects</div>
+        <section className="c-space my-20 ">
+            <p className="head-text">My Work</p>
+        </section>
     )
 }
 export default Projects
