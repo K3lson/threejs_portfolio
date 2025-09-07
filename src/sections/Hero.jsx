7fb413513c4f3c1e67a1fbbda1c2dbd6a38ee6a8
Kelvin Ogboriauphien <kelvinogboriauphien@Kelvins-MacBook-Pro.local>
2025-09-07 13:56:21 +0200

    Initial commit

---
+++ src/sections/Hero.jsx
@@ -0,0 +1,48 @@
+import {Canvas} from "@react-three/fiber";
+import {PerspectiveCamera} from "@react-three/drei";
+import HackerRoom from "../components/HackerRoom.jsx";
+import {Suspense} from "react";
+import CanvasLoader from "../components/CanvasLoader.jsx";
+import useMediaQuery from "react-responsive/src/useMediaQuery.js";
+import {calculateSizes} from "../constants/index.js";
+import Target from "../components/Target.jsx";
+
+const Hero = () => {
+
+    const isMobile = useMediaQuery({ maxWidth: 768 })
+    const isTablet = useMediaQuery({ minWidth: 768, maxWidth: 1024 })
+    const isSmall = useMediaQuery({ maxWidth: 440 })
+    const sizes = calculateSizes(isMobile, isTablet, isSmall);
+    
+                                        
+    return (
+        <section className="min-h-screen w-full flex flex-col relative">
+            <div className="w-full mx-auto flex flex-col sm:mt-36 mt-20 c-space gap-3">
+                <p className="sm:text-3xl text-2xl font-medium text-white text-center font-generalsans ">Hi, I am Kelvin <span className="waving-hand">👋</span></p>
+                <p className="hero_tag text-gray_gradient">Building Responsive and Attractive Apps</p>
+            </div>
+
+            <div className="w-full h-full absolute inset-0 ">
+                {/*<Leva />*/}
+                <Canvas className="w-full h-full">
+                    <Suspense fallback={<CanvasLoader />}>
+                        <PerspectiveCamera makeDefault  position={[0, 0, 20]}/>
+
+                        <HackerRoom
+                            position={sizes.deskPosition}
+                            rotation={[0, -Math.PI, 0 ]}
+                            scale={sizes.deskScale}
+                        />
+                        <group>
+                            <Target position={sizes.targetPosition} />
+                        </group>
+                        <ambientLight intensity={1} />
+                        <directionalLight postion={[10, 10, 10]} intensity={0.5} />
+                    </Suspense>
+                </Canvas>
+
+            </div>
+        </section>
+    )
+}
+export default Hero
